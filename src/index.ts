ec36e1d81662a610c03f4d05b978d958344bded3
Akos <akosegypro@gmail.com>
2021-10-02 14:56:56 +0200

    fix :wrench: script to avoid automatic detection from browsers

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,37 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import cheerio from "cheerio";
+import pretty from "pretty";
+
+//Requried to avoid automatic detection
+puppeteer.use(StealthPlugin());
 
 const url = "https://www.elgiganten.dk/tilbud";
 
 const getPage = async (url: string) => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
   const page = await browser.newPage();
+
+  // Set user agent is neccessary to avoid detection
   page.setUserAgent(
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
   );
+
+  // networkidle property wait until no more network calls are made
   await page.goto(url, { waitUntil: "networkidle0" });
 
   const html = await page.content(); // serialized HTML of page DOM.
+
   await browser.close();
   return html;
 };
 
 const loadHtml = async () => {
   const html = await getPage(url);
-  const $ = cheerio.load(html);
 
-  console.log("🚀", $.html());
+  console.log("HERE: ", pretty(html));
 };
 
 loadHtml();
